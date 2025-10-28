063edb5ebedc1e92d2c2705e8f992bd5047a9636
Nikil2 <nikilgoindani123@gmail.com>
2025-10-29 01:04:22 +0500

    add get and update profile api

---
+++ src/modules/auth/dto/update-profile.dto.ts
@@ -0,0 +1,24 @@
+import { IsString, IsOptional } from 'class-validator';
+
+export class UpdateProfileDto {
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsString()
+  @IsOptional()
+  city?: string;
+
+  @IsString()
+  @IsOptional()
+  country?: string;
+
+  @IsString()
+  @IsOptional()
+  address?: string;
+}
+
